18c535af55180930e9e55aa38af125bd3e67464d
sanjayvarma199 <sanjayvarma199@gmail.com>
2022-02-12 23:31:51 +0530

    Time: 7 ms (70.02%), Space: 47.6 MB (8.83%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
@@ -10,48 +10,19 @@
 class Solution {
     //Time O(N)
     //Space O(H)
-    List<List<TreeNode>> Paths;
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(root == null)
+        if(root == null || root == p || root == q)
+        {
+            return root;
+        }
+        TreeNode Left = lowestCommonAncestor(root.left , p , q);
+        TreeNode Right = lowestCommonAncestor(root.right , p , q);
+        if(Left == null && Right == null)
         {
             return null;
         }
-        Paths = new ArrayList<>();
-        helper(root , p , q, new ArrayList<>());
-        int i = 0;
-        while(Paths.get(0).get(i) == Paths.get(1).get(i))
-        {
-            i++;
-        }
-        return Paths.get(0).get(i-1);
-    }
-    
-    private void helper(TreeNode root , TreeNode p , TreeNode q , List<TreeNode> path)
-    {
-        //base
-        if(root == null)
-        {
-            return;
-        }
-        //Action
-        path.add(root);
-        if(root.val == p.val)
-        {
-            path.add(root);
-            Paths.add(new ArrayList<>(path));
-            path.remove(root);
-        }
-        if(root.val == q.val)
-        {
-            path.add(root);
-            Paths.add(new ArrayList<>(path));
-            path.remove(root);
-        }
-        //Recurse
-        helper(root.left , p , q, path);
-        helper(root.right , p , q, path);
-        
-        //BackTrack
-        path.remove(root);
+        else if(Left != null && Right == null) return Left;
+        else if(Left == null && Right != null) return Right;
+        else return root;
     }
 }
