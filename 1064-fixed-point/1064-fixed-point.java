2cea10cfe76a5d3cc077808187bb2b0f42aa7384
sanjayvarma199 <sanjayvarma199@gmail.com>
2022-02-24 15:06:43 +0530

    Time: 0 ms (100.00%), Space: 45 MB (48.07%) - LeetHub

---
+++ 1064-fixed-point/1064-fixed-point.java
@@ -0,0 +1,32 @@
+class Solution {
+    //Time O(logN)
+    //Space O(1)
+    public int fixedPoint(int[] arr) {
+        if(arr == null || arr.length == 0)
+        {
+            return -1;
+        }
+        int low = 0 , high = arr.length-1;
+        while(low <= high)
+        {
+            int mid = low + (high-low)/2;
+            if(arr[mid] == mid)
+            {
+                while(mid >= 0 && arr[mid] == mid)
+                {
+                    mid--;
+                }
+                return mid+1;
+            }
+            if(arr[mid] < mid)
+            {
+                low = mid+1;
+            }
+            else
+            {
+                high = mid-1;
+            }
+        }
+        return -1;
+    }
+}
