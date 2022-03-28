66c8e65859bb9bf082ee87faa8e4b06d387d283d
sanjayvarma199 <sanjayvarma199@gmail.com>
2022-03-28 09:08:35 +0530

    Time: 1 ms (100.00%), Space: 50.3 MB (93.89%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.java
+++ 238-product-of-array-except-self/238-product-of-array-except-self.java
@@ -1,6 +1,6 @@
 class Solution {
     //Time O(N)
-    //Space O(N)
+    //Space O(1)
     public int[] productExceptSelf(int[] nums) {
         //Input Validation
         if(nums == null || nums.length == 0)
@@ -9,7 +9,6 @@ class Solution {
         }
         int n = nums.length;
         int Left[] = new int[n];
-        int Right[] = new int[n];
         
         Left[0] = 1;
         //For Left Sum
@@ -18,18 +17,14 @@ class Solution {
              Left[i] = Left[i-1]*nums[i-1];   
         }
         
-        Right[n-1] = 1;
+        int Right = 1;
         //For Left Sum
-        for(int i = n-2 ; i >= 0 ; i--)
+        for(int i = n-1 ; i >= 0 ; i--)
         {
-             Right[i] = Right[i+1]*nums[i+1];   
+             Left[i] = Right*Left[i]; 
+             Right = Right * nums[i];
         }
         
-        //Total Product
-        for(int i = 0 ; i < n ; i++)
-        {
-            Left[i] = Left[i] * Right[i];
-        }
         return Left;
     }
 }
