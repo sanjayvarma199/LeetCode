4f26cacd20039bf88e91d8443aab3ef548fa3bee
sanjayvarma199 <sanjayvarma199@gmail.com>
2022-04-05 12:18:49 +0530

    Time: 19 ms (92.61%), Space: 46.2 MB (94.17%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -7,32 +7,30 @@ class Solution {
         {
             return new ArrayList<>();
         }
-        
-        //Sort the input array
-        Arrays.sort(nums);
-        
         List<List<Integer>> result = new ArrayList<>();
         
-        for(int i = 0 ; i < nums.length ; i++)
+        Arrays.sort(nums);
+        
+        for(int i = 0 ; i < nums.length-2 ; i++)
         {
+            //Two pointer approach to find the -nums[i]
             int low = i+1 , high = nums.length-1;
-            //Skip the duplicates
+            
+            //Skip the adjacent elements which are equal to avoid duplicates
             if(i > 0 && nums[i] == nums[i-1]) continue;
-
-            //Two pointers to find -ve sum
             while(low < high)
             {
                 int sum = nums[low] + nums[high];
-                if(nums[i] == -sum)
+                if(sum == -nums[i])
                 {
                     List<Integer> list = new ArrayList<>();
-                    list.add(nums[i]); list.add(nums[low]) ; list.add(nums[high]);
+                    list.add(nums[i]);list.add(nums[low]);list.add(nums[high]);
                     result.add(list);
                     low++;high--;
                     while(low < high && nums[low] == nums[low-1]) low++;
                     while(low < high && nums[high] == nums[high+1]) high--;
                 }
-                else if(nums[i] < -sum)
+                else if(sum < -nums[i])
                 {
                     low++;
                 }
@@ -40,7 +38,6 @@ class Solution {
                 {
                     high--;
                 }
-                
             }
         }
         return result;
