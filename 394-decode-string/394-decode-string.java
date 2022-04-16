3eefab13eb67c744aa04aac7a4e768a5c2170992
sanjayvarma199 <sanjayvarma199@gmail.com>
2022-04-16 12:00:19 +0530

    Time: 1 ms (84.27%), Space: 39.8 MB (99.45%) - LeetHub

--- 394-decode-string/394-decode-string.java
+++ 394-decode-string/394-decode-string.java
@@ -1,51 +1,49 @@
 class Solution {
-    //Time O(N*k) k is max num
-    //Space O(Depth of nested braces)
+    //Time O(N*K) k -> Max num
+    //Space O(Depth)
     public String decodeString(String s) {
-        //Input Validation
         if(s == null || s.length() == 0)
         {
             return s;
         }
         
-        Stack<StringBuilder> string_stack = new Stack<>();
         Stack<Integer> num_stack = new Stack<>();
+        Stack<StringBuilder> string_stack = new Stack<>();
         
         int num = 0;
         StringBuilder sb = new StringBuilder();
+        
         for(int i = 0 ; i < s.length() ; i++)
         {
             char c = s.charAt(i);
             
-            if(c == '[')
+            if(Character.isDigit(c))
+            {
+                num = num*10 + (c-'0');
+            }
+            else if(c == '[')
             {
                 num_stack.push(num);
                 string_stack.push(sb);
                 
-                num = 0;
                 sb = new StringBuilder();
+                num = 0;
             }
             else if(c == ']')
             {
-                int curr = num_stack.pop();
-                StringBuilder curr_string = new StringBuilder(sb.toString());
-                sb = new StringBuilder();
-                for(int j = 0 ; j < curr ; j++)
+                StringBuilder curr = new StringBuilder();
+                int curr_num = num_stack.pop();
+                for(int j = 0 ; j < curr_num ; j++)
                 {
-                    sb.append(curr_string);
+                    curr.append(sb);
                 }
-                sb = string_stack.pop().append(sb);
+                sb = new StringBuilder(string_stack.pop().append(curr));
             }
-            else if(Character.isDigit(c))
-            {
-                num = num*10 + (c-'0');
-            }
-            else 
+            else
             {
                 sb.append(c);
             }
         }
-        
         return sb.toString();
     }
 }
