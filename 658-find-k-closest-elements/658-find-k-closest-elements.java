c5ce392d08b9d1eb874879ac5c5f7e43f325b0cf
sanjayvarma199 <sanjayvarma199@gmail.com>
2022-04-21 16:22:28 +0530

    Time: 4 ms (86.87%), Space: 62.9 MB (28.82%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.java
+++ 658-find-k-closest-elements/658-find-k-closest-elements.java
@@ -1,63 +1,36 @@
 class Solution {
-    //Time O(logN + K)
-    //Space O(1)
-    int left = 0 , right = 0;
+    //Time O(log(N-k) + K)
+    //Space O(K)
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
-        //Input Validation
+        //Null Case
         if(arr == null || arr.length == 0)
         {
             return new ArrayList<>();
         }
         
-        BinarySearch(arr , x);
+        int low = 0 , high = arr.length-k;
         
-        left = left-1;
-        right = left+1;
-        
-        while(right - left <= k)
+        while(low < high)
         {
-            //If it reaches the left Bound
-            if(left < 0)
-            {
-                right++;
-                continue;
-            }
+            int mid = low + (high - low)/2;
+            int Dist_L = x - arr[mid];
+            int Dist_R = arr[mid+k] - x;
             
-            if(right == arr.length || (Math.abs(arr[left] - x)  <= Math.abs(arr[right]-x)))
+            if(Dist_L > Dist_R)
             {
-                left--;
+                low = mid+1;
             }
             else
             {
-                right++;
+                high = mid;
             }
         }
         
         List<Integer> result = new ArrayList<>();
-        for(int i = left+1 ; i < right ; i++)
+        for(int i = low ; i < low+k ; i++)
         {
             result.add(arr[i]);
         }
         return result;
-    }
-    
-    private void BinarySearch(int[] arr , int x)
-    {
-        left = 0 ; right = arr.length-1;
-        
-        while(left < right)
-        {
-            int mid = left + (right-left)/2;
-            
-            if(arr[mid] >= x)
-            {
-                right = mid;
-            }
-            else
-            {
-                left = mid+1;
-            }
-        }
-        return;
     }
 }
