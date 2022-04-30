815f3a15c1a6b787af3318ce0a925ed8bb01efa4
sanjayvarma199 <sanjayvarma199@gmail.com>
2022-04-30 08:05:04 +0530

    Time: 30 ms (41.75%), Space: 48.4 MB (32.49%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.java
@@ -1,41 +1,45 @@
 class Solution {
     //Time O(N)
     //Space O(1)
-    Map<Character , Integer> p_map;
     public List<Integer> findAnagrams(String s, String p) {
         if(s == null || s.length() == 0)
         {
             return new ArrayList<>();
         }
-        int i = 0 , j = 0;
-        List<Integer> list = new ArrayList<>();
-        
-        p_map = new HashMap<>();
         Map<Character , Integer> map = new HashMap<>();
-        
-        for(int k = 0 ; k < p.length() ; k++)
+        for(int i = 0 ; i < p.length() ; i++)
         {
-            char c = p.charAt(k);
-            p_map.put(c , p_map.getOrDefault(c , 0)+1);
-        }
-        
-        while(j < s.length())
-        {
-            char c = s.charAt(j);
+            char c = p.charAt(i);
             map.put(c , map.getOrDefault(c , 0)+1);
-            if(j >= p.length())
-            {
-                map.put(s.charAt(i) , map.get(s.charAt(i))-1);
-                map.remove(s.charAt(i) , 0);
-                i++;
-            }
-            if(map.equals(p_map))
-            {
-                    list.add(i);
-            }
-            
-            j++;
         }
-        return list;
+        
+        int match = map.size() , j = 0;
+        List<Integer> result = new ArrayList<>();
+        for(int i = 0 ; i < s.length() ; i++)
+        {
+            char c_in = s.charAt(i);
+            if(map.containsKey(c_in))
+            {
+                map.put(c_in , map.get(c_in)-1);
+                if(map.get(c_in) == 0)
+                {
+                    match--;
+                }
+            }
+            if(i >= p.length())
+            {
+                char c_out = s.charAt(i-p.length());
+                if(map.containsKey(c_out))
+                {
+                    map.put(c_out , map.get(c_out)+1);
+                    if(map.get(c_out) == 1)
+                    {
+                        match++;
+                    }
+                }
+            }
+            if(match == 0) result.add(i-p.length()+1);
+        }
+        return result;
     }
 }
