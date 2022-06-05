ee96fe024053751507d3bbfdcd6922dd3a09924f
sanjayvarma199 <sanjayvarma199@gmail.com>
2022-06-05 10:52:49 +0530

    Time: 18 ms (17.33%), Space: 55 MB (56.35%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.java
+++ 56-merge-intervals/56-merge-intervals.java
@@ -1,30 +1,31 @@
 class Solution {
     //Time O(NlogN)
-    //Space O(1)
+    //Space O(N)
     public int[][] merge(int[][] intervals) {
         if(intervals == null || intervals.length == 0)
         {
             return intervals;
         }
-        //Sort the intervals based on start time
+        
         Arrays.sort(intervals , new Comparator<>()
                     {
                         public int compare(int[] a , int[] b)
                         {
-                            return a[0]-b[0];
+                            return a[0] - b[0];
                         }
                     });
+        
         LinkedList<int[]> list = new LinkedList<>();
         
-        for(int i = 0 ; i < intervals.length ; i++)
+        for(int[] ar : intervals)
         {
-            if(!list.isEmpty() && list.getLast()[1] >= intervals[i][0])
+            if(!list.isEmpty() && list.getLast()[1] >= ar[0])
             {
-                list.getLast()[1] = Math.max(list.getLast()[1] , intervals[i][1]);
+                list.getLast()[1] = Math.max(list.getLast()[1] , ar[1]);
             }
             else
             {
-                list.add(intervals[i]);
+                list.add(ar);
             }
         }
         return list.toArray(new int[list.size()][2]);
