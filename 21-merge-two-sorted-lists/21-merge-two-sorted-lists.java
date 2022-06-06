1e1bcf2432692844035dae8a5557d10e41584a24
sanjayvarma199 <sanjayvarma199@gmail.com>
2022-06-06 12:26:27 +0530

    Time: 1 ms (66.79%), Space: 43.4 MB (26.66%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.java
@@ -9,18 +9,19 @@
  * }
  */
 class Solution {
-    //Time O(Max(l1 , l2))
-    //Space O(1)
-    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        if(l1 == null && l2 == null)
+    //Time O(M+N)
+    //Space O(M+N)
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if(list1 == null && list2 == null)
         {
-            return null;
+            return list1;
         }
-        ListNode dummy = new ListNode(-1);
-        ListNode head = dummy;
+        ListNode head = new ListNode(-1);
+        ListNode l1 = list1 , l2 = list2 , ans = head;
+        
         while(l1 != null && l2 != null)
         {
-            if(l1.val <= l2.val)
+            if(l1.val < l2.val)
             {
                 head.next = new ListNode(l1.val);
                 l1 = l1.next;
@@ -28,12 +29,14 @@ class Solution {
             else
             {
                 head.next = new ListNode(l2.val);
-                l2 = l2.next;
+                l2  = l2.next;
             }
+            
             head = head.next;
         }
         if(l1 != null) head.next = l1;
         if(l2 != null) head.next = l2;
-        return dummy.next;
+        
+        return ans.next;
     }
 }
