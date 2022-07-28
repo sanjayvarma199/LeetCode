efacc6e7ab42ba3bd9f1e5ee6231004694b4d4d5
sanjayvarma199 <sanjayvarma199@gmail.com>
2022-07-28 13:21:50 +0530

    Time: 27 ms (81.71%), Space: 59 MB (51.67%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -7,36 +7,35 @@ class Solution {
         {
             return new ArrayList<>();
         }
-        List<List<Integer>> result = new ArrayList<>();
         
+        List<List<Integer>> result = new ArrayList<>();
         Arrays.sort(nums);
         
         for(int i = 0 ; i < nums.length-2 ; i++)
         {
-            //Two pointer approach to find the -nums[i]
-            int low = i+1 , high = nums.length-1;
-            
-            //Skip the adjacent elements which are equal to avoid duplicates
+            int j = i+1 , k = nums.length-1;
             if(i > 0 && nums[i] == nums[i-1]) continue;
-            while(low < high)
+            while(j < k)
             {
-                int sum = nums[low] + nums[high];
-                if(sum == -nums[i])
+                int sum = nums[j] + nums[k];
+                if(nums[i] == -sum)
                 {
-                    List<Integer> list = new ArrayList<>();
-                    list.add(nums[i]);list.add(nums[low]);list.add(nums[high]);
-                    result.add(list);
-                    low++;high--;
-                    while(low < high && nums[low] == nums[low-1]) low++;
-                    while(low < high && nums[high] == nums[high+1]) high--;
+                    List<Integer> temp = new ArrayList<>();
+                    temp.add(nums[i]);temp.add(nums[j]);temp.add(nums[k]);
+                    result.add(temp);
+                    j++;
+                    k--;
+                    //To avoid duplicates
+                    while(j < k && nums[j-1] == nums[j]) j++;
+                    while(j < k && nums[k] == nums[k+1]) k--;
                 }
-                else if(sum < -nums[i])
+                else if(nums[i] < -sum)
                 {
-                    low++;
+                    j++;
                 }
                 else
                 {
-                    high--;
+                    k--;
                 }
             }
         }
