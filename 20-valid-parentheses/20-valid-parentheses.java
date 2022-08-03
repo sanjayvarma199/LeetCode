52ac16d67935fd33b932968f0d39b85f5728f7c0
sanjayvarma199 <sanjayvarma199@gmail.com>
2022-08-03 14:23:12 +0530

    Time: 5 ms (15.38%), Space: 41.8 MB (74.40%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.java
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -2,30 +2,31 @@ class Solution {
     //Time O(N)
     //Space O(1)
     public boolean isValid(String s) {
+        //Input Validation
         if(s == null || s.length() == 0)
         {
-            return false;
+            return true;
         }
         Stack<Character> stack = new Stack<>();
         Map<Character , Character> map = new HashMap<>();
-        map.put('}','{');
         map.put(')','(');
         map.put(']','[');
+        map.put('}','{');
         
         for(int i = 0 ; i < s.length() ; i++)
         {
             char c = s.charAt(i);
-            if(map.containsKey(c))
+            if(!map.containsKey(c))
             {
-                char pop = stack.isEmpty() ? '#' : stack.pop();
-                if(map.get(c) != pop)
-                {
-                    return false;
-                }
+                stack.push(c);
             }
             else
             {
-                stack.push(c);
+                char pop = stack.isEmpty() ? '#' : stack.pop();
+                if(pop != map.get(c))
+                {
+                    return false;
+                }
             }
         }
         return stack.isEmpty();
