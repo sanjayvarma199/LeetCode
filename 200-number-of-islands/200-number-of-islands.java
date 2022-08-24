81f7716bb256094c2a19891b312ee7835d56f00c
sanjayvarma199 <sanjayvarma199@gmail.com>
2022-08-24 17:04:59 +0530

    Time: 11 ms (18.98%), Space: 56.3 MB (77.62%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.java
+++ 200-number-of-islands/200-number-of-islands.java
@@ -1,13 +1,14 @@
 class Solution {
-    //Time O(M*N)
-    //Space O(Max(M,N))
+    //TC O(M*N)
+    //SC O(Min(M,N))
     public int numIslands(char[][] grid) {
+        //Inout Validation
         if(grid == null || grid.length == 0)
         {
             return 0;
         }
         
-        int islands = 0;
+        int ans = 0;
         for(int i = 0 ; i < grid.length ; i++)
         {
             for(int j = 0 ; j < grid[0].length ; j++)
@@ -15,33 +16,35 @@ class Solution {
                 if(grid[i][j] == '1')
                 {
                     BFS(grid , i , j);
-                    islands++;
+                    ans++;
                 }
             }
         }
-        return islands;
+        return ans;
     }
-    private void BFS(char[][] grid , int i , int j)
+    
+    private void BFS(char[][] grid , int i, int j)
     {
-        int dirs[][] = {{0,1},{0,-1},{1,0},{-1,0}};
-        Queue<int[]> Q = new LinkedList<>();
+        int[][] dirs = {{0,1},{0,-1},{1,0},{-1,0}};
         
-        Q.add(new int[] {i,j});
-        grid[i][j] = '0';
+        Queue<int[]> Q = new LinkedList<>();
+        Q.add(new int[]{i,j});
+        grid[i][j] = '0'; // Make it visited
         
         while(!Q.isEmpty())
         {
             int[] curr = Q.poll();
             for(int[] dir : dirs)
             {
-                int new_i = dir[0] + curr[0];
-                int new_j = dir[1] + curr[1];
+                int new_i = curr[0] + dir[0];
+                int new_j = curr[1] + dir[1];
                 if(new_i >= 0 && new_j >= 0 && new_i < grid.length && new_j < grid[0].length && grid[new_i][new_j] == '1')
                 {
                     grid[new_i][new_j] = '0';
-                    Q.add(new int[] {new_i , new_j});
+                    Q.add(new int[]{new_i , new_j});
                 }
             }
         }
+        return;
     }
 }
