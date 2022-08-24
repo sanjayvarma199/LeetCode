ed11ddb006c9463bc9ad1589a5972e14f7657da7
sanjayvarma199 <sanjayvarma199@gmail.com>
2022-08-25 02:08:02 +0530

    Time: 48 ms (55.72%), Space: 118.7 MB (23.17%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
@@ -1,62 +1,30 @@
 class Solution {
-    //Time amortized O(N)
-    //Space O(1)
-    int k;
+    //TC O(NlogN)
+    //SC O(K)
     public int[][] kClosest(int[][] points, int k) {
+        //Input Validation
         if(points == null || points.length == 0)
         {
             return points;
         }
-        this.k = k;
-        return QuickSelect(points);
-    }
-    private int[][] QuickSelect(int[][] points)
-    {
-        int low = 0 , high = points.length-1 , pivot = points.length;
-        while(pivot != k)
+        
+        PriorityQueue<int[]> PQ = new PriorityQueue<>((a,b) -> (-(a[0]*a[0] + a[1]*a[1]) + (b[0]*b[0] + b[1]*b[1])));
+        
+        for(int i = 0 ; i < points.length ; i++)
         {
-            pivot = partition(points , low , high);
-            if(pivot < k)
+            PQ.add(points[i]);
+            if(PQ.size() > k)
             {
-                low = pivot;
-            }
-            else
-            {
-                high = pivot-1;
+                PQ.poll();
             }
         }
-        return Arrays.copyOf(points , k);
-    }
-    private int partition(int[][] points , int low , int high)
-    {
-        int[] pivot = points[low + (high-low)/2];
-        while(low < high)
+        int[][] ans = new int[k][2];
+        int i = k-1;
+        while(!PQ.isEmpty())
         {
-            if(distance(points[low]) >= distance(pivot))
-            {
-                swap(points , low , high);
-                high--;
-            }
-            else
-            {
-                low++;
-            }
+            ans[i] = PQ.poll();
+            i--;
         }
-        if(distance(points[low]) < distance(pivot))
-        {
-            low++;
-        }
-        return low;
-    }
-    private void swap(int[][] points, int i , int p)
-    {
-                int[] temp = points[i];
-                points[i] = points[p];
-                points[p] = temp;
-                p++;
-    }
-    private int distance(int[] point)
-    {
-        return point[0]*point[0] + point[1]*point[1];
+        return ans;
     }
 }
