ba5e1cf2aa3a9c5739c0844cf6df684b8dfc7f96
sanjayvarma199 <sanjayvarma199@gmail.com>
2022-11-05 16:31:10 -0400

    Time: 8 ms (57.25%), Space: 45.1 MB (47.95%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
@@ -1,40 +1,34 @@
 class Solution {
-    // Return true if the character is a vowel (case-insensitive)
-    boolean isVowel(char c) {
-        return c == 'a' || c == 'i' || c == 'e' || c == 'o' || c == 'u'
-            || c == 'A' || c == 'I' || c == 'E' || c == 'O' || c == 'U';
-    }
-    
-    // Function to swap characters at index x and y
-    void swap(char[] chars, int x, int y) {
-        char temp = chars[x];
-        chars[x] = chars[y];
-        chars[y] = temp;
-    }
-    
+    //TC O(N)
+    //SC O(1)
     public String reverseVowels(String s) {
-        int start = 0;
-        int end  = s.length() - 1;
-        // Convert String to char array as String is immutable in Java
-        char[] sChar = s.toCharArray();
-        
-        // While we still have characters to traverse
-        while (start < end) {
-            // Find the leftmost vowel
-            while (start < s.length () && !isVowel(sChar[start])) {
-                start++;
-            }
-            // Find the rightmost vowel
-            while (end >= 0 && !isVowel(sChar[end])) {
-                end--;
-            }
-            // Swap them if start is left of end
-            if (start < end) {
-                swap(sChar, start++, end--);
-            }
+        //Input Validation
+        if(s == null || s.length() == 0)
+        {
+            return s;
         }
+        Set<Character> set = new HashSet<>();
+        set.add('a');set.add('e');set.add('i');set.add('o');set.add('u');
+        set.add('A');set.add('E');set.add('I');set.add('O');set.add('U');
         
-        // Converting char array back to String
-        return new String(sChar);
+        int low = 0 , high = s.length()-1;
+        char ar[] = s.toCharArray();
+        while(low < high)
+        {
+            while(low < high && !set.contains(ar[low]))
+            {
+                low++;
+            }
+            while(low < high && !set.contains(ar[high]))
+            {
+                high--;
+            }
+            char temp = ar[low];
+            ar[low] = ar[high];
+            ar[high] = temp;
+            low++;
+            high--;
+        }
+        return String.valueOf(ar);
     }
-};
+}
