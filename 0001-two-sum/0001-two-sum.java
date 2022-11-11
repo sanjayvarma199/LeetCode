31995d8815feccdc15ae76c18c2252b3f5db0a78
sanjayvarma199 <sanjayvarma199@gmail.com>
2022-11-11 12:10:16 -0500

    Time: 9 ms (58.55%), Space: 45.9 MB (26.88%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.java
@@ -0,0 +1,24 @@
+class Solution {
+    //TC O(N)
+    //SC O(1)
+    public int[] twoSum(int[] nums, int target) {
+        //Input Validation
+        if(nums == null || nums.length == 0)
+        {
+            return new int[]{-1,-1};
+        }
+        //To Maintain the compliement of the target
+        Map<Integer,Integer> map = new HashMap<>();
+        
+        for(int i = 0 ; i < nums.length ; i++)
+        {
+            int compliment = target-nums[i];
+            if(map.containsKey(compliment))
+            {
+                return new int[] {map.get(compliment) , i};
+            }
+            map.put(nums[i] , i);
+        }
+        return new int[] {-1,-1};
+    }
+}
